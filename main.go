c8bc73f816ab7e0a5ed626b964a5f59567f44cdd
seyukun <yus-sato@kalyte.ro>
2025-03-08 19:02:06 +0900

    wip

--- main.go
+++ main.go
@@ -22,12 +22,13 @@ func main() {
 		log.Panic(err)
 	}
 	dev := device.NewDevice(tun, conn.NewDefaultBind(), device.NewLogger(device.LogLevelVerbose, ""))
-	dev.IpcSet(`private_key=
-public_key=
+	dev.IpcSet(`private_key=087ec6e14bbed210e7215cdc73468dfa23f080a1bfb8665b2fd809bd99d28379
 listen_port=58120
+public_key=c4c8e984c5322c8184c72265b92b250fdb63688705f504ba003c88f03393cf28
 allowed_ip=10.0.0.20/32
 persistent_keepalive_interval=25
- `)
+endpoint=153.127.192.151:58120
+`)
 	dev.Up()
 	listener, err := tnet.ListenTCP(&net.TCPAddr{Port: 80})
 	if err != nil {
