2143fe49215f51dd62b5e0187ed604c8b92006b9
seyukun <yus-sato@kalyte.ro>
2025-03-12 00:06:17 +0900

    update

--- main.go
+++ main.go
@@ -16,15 +16,7 @@ func main() {
 		log.Panic(err)
 	}
 	dev := device.NewDevice(tun, conn.NewDefaultBind(), device.NewLogger(device.LogLevelVerbose, ""))
-	dev.IpcSet(`
-private_key=087ec6e14bbed210e7215cdc73468dfa23f080a1bfb8665b2fd809bd99d28379
-listen_port=58120
-
-public_key=c4c8e984c5322c8184c72265b92b250fdb63688705f504ba003c88f03393cf28
-allowed_ip=10.0.0.20/32
-persistent_keepalive_interval=25
-endpoint=153.127.192.151:58120
-`)
+	dev.IpcSet(``)
 	dev.Up()
 
 	sig := make(chan os.Signal, 1)
