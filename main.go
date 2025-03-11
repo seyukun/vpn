0fb6a774c7c1bd57d594d5d64353a4192eb2edbe
seyukun <yus-sato@kalyte.ro>
2025-03-12 01:04:10 +0900

    update

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -16,7 +17,17 @@ func main() {
 		log.Panic(err)
 	}
 	dev := device.NewDevice(tun, conn.NewDefaultBind(), device.NewLogger(device.LogLevelVerbose, ""))
-	dev.IpcSet(``)
+	if file, err := os.OpenFile(".config", os.O_RDONLY, 0); err != nil {
+		log.Panic(err)
+	} else {
+		fbytes, err := io.ReadAll(file)
+		file.Close()
+		if err != nil {
+			log.Panic(err)
+		} else {
+			dev.IpcSet(string(fbytes))
+		}
+	}
 	dev.Up()
 
 	sig := make(chan os.Signal, 1)
