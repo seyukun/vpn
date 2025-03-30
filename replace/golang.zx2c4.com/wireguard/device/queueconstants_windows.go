807c2cad0289c333773eb78f5e4448421a460312
seyukun <yus-sato@kalyte.ro>
2025-03-30 17:54:57 +0900

    update

--- replace/golang.zx2c4.com/wireguard/device/queueconstants_windows.go
+++ replace/golang.zx2c4.com/wireguard/device/queueconstants_windows.go
@@ -6,10 +6,12 @@
 package device
 
 const (
-	QueueStagedSize            = 128
-	QueueOutboundSize          = 1024
-	QueueInboundSize           = 1024
-	QueueHandshakeSize         = 1024
+	QueueStagedSize    = 128
+	QueueOutboundSize  = 1024
+	QueueInboundSize   = 1024
+	QueueHandshakeSize = 1024
+	/* ADDON  QueueStunSize  = 1024*/
+	QueueStunSize              = 1024
 	MaxSegmentSize             = 2048 - 32 // largest possible UDP datagram
 	PreallocatedBuffersPerPool = 0         // Disable and allow for infinite memory growth
 )
